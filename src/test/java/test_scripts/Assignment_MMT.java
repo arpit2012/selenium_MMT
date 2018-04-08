b12383ca54eb4cdfd9f4ee70f396d09abd63718a
Arpit <Arpit@192.168.1.4>
2018-04-08 11:26:07 +0530

    [with extent reports]

--- src/test/java/test_scripts/Assignment_MMT.java
+++ src/test/java/test_scripts/Assignment_MMT.java
@@ -1,5 +1,6 @@
 package test_scripts;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.List;
 import java.util.Properties;
@@ -12,7 +13,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class Assignment_MMT {
 
@@ -25,6 +35,21 @@ public class Assignment_MMT {
 	Properties EnterPassengerDetailsUIProp = new Properties();
 	Properties PaymentDetailsUIProp = new Properties();
 	Properties dataProp = new Properties();
+	ExtentReports extent;
+	ExtentTest logger;
+
+	@BeforeTest
+	public void startReport(){
+
+		extent = new ExtentReports (System.getProperty("user.dir")+"/test-output/STMExtentReport.html", true);
+
+		extent
+		.addSystemInfo("Host Name", "Mac")
+		.addSystemInfo("Application", "MakeMyTrip.com")
+		.addSystemInfo("User Name", "Arpit");
+
+		extent.loadConfig(new File(System.getProperty("user.dir")+"/extent-config.xml"));
+	}
 
 	public void login() throws Exception{
 
@@ -101,9 +126,7 @@ public class Assignment_MMT {
 
 		for(WebElement ele:allDates)
 		{
-
 			String date=ele.getText();
-
 			if(date.contains(dataProp.getProperty("date")))
 			{
 				ele.click();
@@ -139,17 +162,17 @@ public class Assignment_MMT {
 	public void reviewPage() throws Exception{
 		FileInputStream fisReviewPageUI=new FileInputStream("/Users/Arpit/Documents/Eclipse_Worksapce/mavenTestNG/src/main/resources/objectRepository/ReviewPage.property");
 		ReviewPageUIProp.load(fisReviewPageUI);
-		
+
 		wd.findElement(By.id(ReviewPageUIProp.getProperty("reviewPageContinueToReview"))).click();
 		System.out.println("Clicked on Continue Button");
 		Thread.sleep(1000);
 	}
 
 	public void enterPassangerDetails() throws Exception{
-		
+
 		FileInputStream fisPassengerDetailsUI=new FileInputStream("/Users/Arpit/Documents/Eclipse_Worksapce/mavenTestNG/src/main/resources/objectRepository/EnterPassengerDetails.property");
 		EnterPassengerDetailsUIProp.load(fisPassengerDetailsUI);
-		
+
 		wd.findElement(By.xpath(EnterPassengerDetailsUIProp.getProperty("enterPassengerDetails_firstName"))).sendKeys(dataProp.getProperty("firstName"));
 		wd.findElement(By.xpath(EnterPassengerDetailsUIProp.getProperty("enterPassengerDetails_lastName"))).sendKeys(dataProp.getProperty("lastName"));
 		wd.findElement(By.xpath(EnterPassengerDetailsUIProp.getProperty("enterPassengerDetails_Mobile"))).sendKeys(dataProp.getProperty("Mobile"));
@@ -165,11 +188,11 @@ public class Assignment_MMT {
 		Thread.sleep(15000);
 
 	}
-	
+
 	public void paymentDetails() throws Exception{
 		FileInputStream fisPaymentDetailsUI=new FileInputStream("/Users/Arpit/Documents/Eclipse_Worksapce/mavenTestNG/src/main/resources/objectRepository/PaymentDetails.property");
 		PaymentDetailsUIProp.load(fisPaymentDetailsUI);
-		
+
 		wd.findElement(By.id(PaymentDetailsUIProp.getProperty("payemnetDetails_PaymentMethod"))).click();
 		System.out.println("Clicked on Credit/Debit Cards");
 
@@ -186,9 +209,9 @@ public class Assignment_MMT {
 		wd.findElement(By.id(PaymentDetailsUIProp.getProperty("payemnetDetails_ChooseToSaveCardDetails"))).click();
 		wd.findElement(By.id(PaymentDetailsUIProp.getProperty("payemnetDetails_ProceedToPay"))).click();
 	}
-	
+
 	@Test
-	public void MMT() throws Exception{
+	public void MMT_end_to_end() throws Exception{
 		FileInputStream fisData=new FileInputStream("/Users/Arpit/Documents/Eclipse_Worksapce/mavenTestNG/src/main/resources/DataMAP/Data.property");
 		dataProp.load(fisData);
 
@@ -198,26 +221,52 @@ public class Assignment_MMT {
 		wd.get(dataProp.getProperty("URL"));
 		wd.manage().window().maximize();
 
+		logger = extent.startTest("MMT_end_to_end");
+
 		login();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_Login");
 		search();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_Search");
 		selectFlight();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_SelectFlight");
 		reviewPage();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_ReviewPage");
 		enterPassangerDetails();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_EnterPassangerDetails");
 		paymentDetails();
+		logger.log(LogStatus.PASS, "Test Case Passed is MMT_PaymentDetails");
+	}
+
+	@AfterMethod
+	public void getResult(ITestResult result){
+		if(result.getStatus() == ITestResult.FAILURE){
+
+
+			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getName());
+			logger.log(LogStatus.FAIL, "Test Case Failed is "+result.getThrowable());
+		}else if(result.getStatus() == ITestResult.SKIP){
+			logger.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+		}
+		// ending test
+		//endTest(logger) : It ends the current test and prepares to create HTML report
+		extent.endTest(logger);
+	}
+
+	@AfterTest
+	public void endReport() throws InterruptedException{
+
+		System.setProperty("webdriver.gecko.driver", "/Users/Arpit/Documents/Eclipse_Worksapce/java_Project_3RI/ExecutableFile/geckodriver");
+		System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/Users/Arpit/Documents/Selenium3_Jars/output");
+		
+		Thread.sleep(3000);
+		wd=new FirefoxDriver();
+		wd.manage().window().maximize();
+		wd.get("file:///Users/Arpit/Documents/Eclipse_Worksapce/mavenTestNG/test-output/STMExtentReport.html") ; 
+		wd.navigate().refresh();
+		extent.flush();
+		extent.close();
 	}
 }
 
 //Arpit Agrawal
 
-//utility.ScreenshotCode.screenShot();
-		//	boolean popupPresent=wait.until(ExpectedConditions.elementToBeClickable(By.id("failed_wallet_got_it"))).isDisplayed();
-
-		//	System.out.println(popupPresent);
-		//		if(popupPresent ==  true)
-		//		{
-		//			wd.findElement(By.id("failed_wallet_got_it")).click();
-		//		}
-
-
-
-		//wd.quit();
